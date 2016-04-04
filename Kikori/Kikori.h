249da5cb6451866f8cdc21479c4e869acfe1602f
Eugene Ovchynykov <nerohellier@gmail.com>
2016-04-04 16:36:03 +0300

    Initial Commit

---
+++ Kikori/Kikori.h
@@ -0,0 +1,19 @@
+//
+//  Kikori.h
+//  Kikori
+//
+//  Created by eugene on 4/4/16.
+//  Copyright © 2016 Eugene Ovchynnykov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kikori.
+FOUNDATION_EXPORT double KikoriVersionNumber;
+
+//! Project version string for Kikori.
+FOUNDATION_EXPORT const unsigned char KikoriVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kikori/PublicHeader.h>
+
+
